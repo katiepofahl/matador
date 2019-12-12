70856eb64cfbc6e29584d40650dee2405548bc01
Katie Pofahl <kapofahl@gmail.com>
2019-12-12 14:55:12 -0500

    updated .csv cell, updated plots, added final ggplot 2

--- Matador Project Script V1.R
+++ Matador Project Script V1.R
@@ -25,21 +25,24 @@ filter(mydata, type == "matador")
 g <- filter(mydata, type == "grassbank")
 mean(g$ranch_acres)
 
-#Subset data
+#Subset Data
 gb_conserved <- select(filter(mydata, type == "grassbank"), c(year,grasslands_conserved))
 gb_protected <- select(filter(mydata, type == "grassbank"), c(year,grasslands_protected))
 gb_conserved_protected <- select(filter(mydata, type == "grassbank"), 
       c(year,grasslands_protected,grasslands_conserved))
+
+#View data
+gb_conserved
+gb_protected
+gb_conserved_protected
 
 #Construct test plots
 lm(gb_conserved) 
 plot(gb_conserved)
 lm(gb_protected) 
 plot(gb_protected)
-plot(gb_conserved_protected$grasslands_protected,
-     gb_conserved_protected$grasslands_conserved)
 
-#Construct alternate plot 1 - acres of conserved lands over time
+#Construct ggplot test plot - acres of conserved lands over time
 p <- ggplot(data = gb_conserved_protected, 
             mapping = aes(x = year, y = grasslands_conserved))+
             geom_point(alpha=0.2)+
@@ -47,9 +50,9 @@ p <- ggplot(data = gb_conserved_protected,
             theme_bw()
 p
 
-#Construct alternate plot 2 - acres of conserved lands and acres of protected lands over time
+#Construct final ggplot 1 - acres of conserved lands and acres of protected lands over time
 p <- ggplot(data = gb_conserved_protected, 
-            mapping = aes(year))+
+            mapping = aes(x = year))+
             geom_smooth(aes(y = grasslands_conserved, colour="conserved"))+
             geom_smooth(aes(y = grasslands_protected, colour="protected"))+
             scale_colour_manual(values=c("blue", "green"))+
@@ -58,11 +61,31 @@ p <- ggplot(data = gb_conserved_protected,
             theme(legend.position="top", legend.title = element_blank())
 p
 
-#Construct alternate plot 3 - dollars paid per acre of program land (return on investment)
-mydata %>% 
-  mutate(cost_per_acre = discounts/(approved_management+grasslands_conserved+
-    grasslands_protected+prairie_dog))
+#Construct final ggplot 2 - dollars paid per acre of program land (return on investment)
 
+## Turn NAs into 0s
+mydata2 <- mydata %>% replace_na(list(approved_management = 0, 
+      grasslands_conserved = 0, grasslands_protected = 0, friendly_fence = 0, discounts = 0))
+mydata2
+
+## Create cost_per_acre variable
+mydata2 <- mydata2 %>%
+  mutate(cost_per_acre = discounts/(approved_management+grasslands_conserved+
+      grasslands_protected+prairie_dog))
+mydata2
+
+##Construct plot
+p <- ggplot(data = mydata2, 
+            mapping = aes(x=year, y=cost_per_acre, color=type))+
+            geom_smooth(method = "loess") +
+            geom_point()+
+            scale_colour_manual(values=c("red", "tan"))+
+            labs(x = "year", y = "discount per acre ($)", 
+              title= "Matador Partners: Discount Per Acre of Conserved Land", 
+              caption="Source: TNC Montana")+
+            theme(legend.position="top", legend.title = element_blank())
+p
 
 
 #####
+
